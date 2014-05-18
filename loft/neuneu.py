50db42d47f311aadc97d84fcb1d801d4b7ca9949
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2014-05-18 14:44:37 +0200

    Added some documentation

--- loft/neuneu.py
+++ loft/neuneu.py
@@ -3,9 +3,21 @@ import numpy as np
 
 class Neuneu(object):
     def __init__(self, energy=10):
+        """
+        Creates a Neuneu
+
+        Parameters
+        ==========
+
+        energy : int, optional, default: 10
+
+        """
         self.energy = energy
         self.position = np.random.randint(0, 100, 2)
 
     def move(self):
+        """
+        Moves in a random direction
+        """
         direction = np.random.randint(-5, 5, 2)
         self.position += direction
