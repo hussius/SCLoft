9c18d9f92db9987e40e0bfdc79f1501526186c38
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2014-05-18 16:18:41 +0200

    Added the creation of loft

--- loft/loft.py
+++ loft/loft.py
@@ -1,5 +1,17 @@
+import numpy as np
+
+from loft import case
 
 
 class Loft(object):
-    def __init__(self):
-        pass
+    def __init__(self, width=100, height=100):
+        self.width = width
+        self.height = height
+        loft = []
+        for i in range(width):
+            row = []
+            for j in range(height):
+                row.append(case.Case())
+            loft.append(row)
+
+        loft = np.array(loft)
