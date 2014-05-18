8085b87e2e5ddbb856788d6b626d28e3474fa176
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2014-05-18 16:31:21 +0200

    Added a random state

--- loft/neuneu.py
+++ loft/neuneu.py
@@ -2,9 +2,14 @@ import numpy as np
 
 
 class Neuneu(object):
-    def __init__(self, energy=10):
+    def __init__(self, energy=10, random_state=None):
+        if random_state is None:
+            random_state = np.random.RandomState()
+        if type(random_state) == int:
+            random_state = np.random.RandomState(seed=random_state)
+        self.random_state = random_state
         self.energy = energy
-        self.position = np.random.randint(0, 100, 2)
+        self.position = random_state.randint(0, 100, 2)
 
     def go(self, direction):
         self.position += direction
@@ -12,5 +17,5 @@ class Neuneu(object):
 
 class Eratic(Neuneu):
     def move(self):
-        direction = np.random.randint(-5, 5, 2)
+        direction = self.random_state.randint(-5, 5, 2)
         self.go(direction)
