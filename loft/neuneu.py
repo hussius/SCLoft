74c2f309c8704782012ed87cf66318a9d4f95f92
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2014-05-18 14:48:53 +0200

    Added some attributes to the neuneu

--- loft/neuneu.py
+++ loft/neuneu.py
@@ -5,6 +5,9 @@ class Neuneu(object):
     def __init__(self, energy=10):
         self.energy = energy
         self.position = np.random.randint(0, 100, 2)
+        self.sexe = np.random.randint(0, 12)
+        self.age = 0
+        self.id = 0
 
     def move(self):
         direction = np.random.randint(-5, 5, 2)
